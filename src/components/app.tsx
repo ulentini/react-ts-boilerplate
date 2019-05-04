e405235f066dfa2e5b3f2ea22252bfc07c68c7e6
Umberto Lentini <umberto.lentini@gmail.com>
2019-05-04 18:30:08 +0200

    First version

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-teal-600 h-screen text-white flex items-center">
+      <h1 className="text-center font-bold text-5xl w-full">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
